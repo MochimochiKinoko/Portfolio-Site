544879995f1eb8dcb85824b7abfb07391c379d04
shalankwa <jonathangoodman.j@gmail.com>
2024-06-27 00:16:08 +0900

    responsiveness cleanup

---
+++ assets/js/contact.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    let contactBtn = document.getElementById('contact-btn');
+
+    contactBtn.addEventListener('click', function() {
+        alert('コンタクトフォームはメンテナンス中のため、ただいま送信機能がご使用頂けません。大変ご不便おかけしており、申し訳ございません。');
+    });
+});
